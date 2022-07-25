9476dcfd81dc1f8ef564de8c5c4e3d37cf6c1365
miculnapoleon <aalexandra.tanase@gmail.com>
2022-07-25 23:47:50 +0300

    added footer

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import styles from './footer.module.scss'
+import Image from 'next/image'
+
+const FOOTER_ITEMS = [
+  {
+    alt: "Instagram",
+    icon:<a href="https://www.instagram.com/" target="__blank" className={styles.link}> <Image key={1}  width={20} height={20} src="/images/insta.svg" /> </a>
+  },
+  {
+    alt: "Behance",
+    icon: <a href="https://www.behance.net//" target="__blank" className={styles.linkB}> <Image key={2}  width={20} height={20} src="/images/behance.svg" /></a>,
+  }
+];
+
+const Footer = () => {
+  
+  const renderFooterItems = () => {
+    return FOOTER_ITEMS.map((item, index) => {
+      return <div className={styles.icon} key={index}>
+        {item.icon} 
+      </div>
+  });
+  };
+  return (
+    <div className={styles.iconContainer}>
+        {renderFooterItems()}
+    </div>
+  );
+};
+
+export default Footer;
